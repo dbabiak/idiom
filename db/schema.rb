03228c3e71e5b313f3ff98afa2574718f75ddb04
Daniel Babiak <d.babiak@gmail.com>
2014-07-24 12:07:34 -0700

    switching to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140707045147) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "problems", force: true do |t|
     t.string   "title",         null: false
     t.text     "description",   null: false
@@ -23,8 +26,8 @@ ActiveRecord::Schema.define(version: 20140707045147) do
     t.datetime "updated_at"
   end
 
-  add_index "problems", ["submitter_id"], name: "index_problems_on_submitter_id"
-  add_index "problems", ["title"], name: "index_problems_on_title", unique: true
+  add_index "problems", ["submitter_id"], name: "index_problems_on_submitter_id", using: :btree
+  add_index "problems", ["title"], name: "index_problems_on_title", unique: true, using: :btree
 
   create_table "solution_likes", force: true do |t|
     t.integer  "user_id",     null: false
@@ -33,8 +36,8 @@ ActiveRecord::Schema.define(version: 20140707045147) do
     t.datetime "updated_at"
   end
 
-  add_index "solution_likes", ["solution_id"], name: "index_solution_likes_on_solution_id"
-  add_index "solution_likes", ["user_id"], name: "index_solution_likes_on_user_id"
+  add_index "solution_likes", ["solution_id"], name: "index_solution_likes_on_solution_id", using: :btree
+  add_index "solution_likes", ["user_id"], name: "index_solution_likes_on_user_id", using: :btree
 
   create_table "solutions", force: true do |t|
     t.integer  "problem_id",   null: false
@@ -44,8 +47,8 @@ ActiveRecord::Schema.define(version: 20140707045147) do
     t.datetime "updated_at"
   end
 
-  add_index "solutions", ["problem_id"], name: "index_solutions_on_problem_id"
-  add_index "solutions", ["submitter_id"], name: "index_solutions_on_submitter_id"
+  add_index "solutions", ["problem_id"], name: "index_solutions_on_problem_id", using: :btree
+  add_index "solutions", ["submitter_id"], name: "index_solutions_on_submitter_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string "username",        null: false
@@ -53,7 +56,7 @@ ActiveRecord::Schema.define(version: 20140707045147) do
     t.string "token",           null: false
   end
 
-  add_index "users", ["token"], name: "index_users_on_token"
-  add_index "users", ["username"], name: "index_users_on_username", unique: true
+  add_index "users", ["token"], name: "index_users_on_token", using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
