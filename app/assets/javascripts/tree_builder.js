b61acd65ca0e26efc110e099429913cc4cc0a466
Daniel Babiak <d.babiak@gmail.com>
2014-07-25 14:59:57 -0700

    json is being created correctly. still have to link nodes to problem pages

---
+++ app/assets/javascripts/tree_builder.js
@@ -0,0 +1,55 @@
+//returns a nested json blob for parsing by the visualization library
+App.Tree = function(problems, options) {
+  var root = {
+    'id': '0',
+    'name': 'root',
+    'data': {},
+    'children': []
+  };
+
+  var subRoots = ['recursion', 'fundamentals', 'algorithms'];
+  subRoots.forEach(function(name) {
+    var subRoot = App.Tree.rootNode(name, 0);
+    root.children.push(subRoot);
+    App.Tree.makeSubTree(subRoot, problems);
+  });
+
+  return root;
+}
+
+App.Tree.makeSubTree = function(root, problems) {
+  // add all problem children
+  problems.forEach(function(problem) {
+    if (problem.rating === root.data.rating && problem.tag === root.name) {
+      root.children.push(problem);
+    }
+  });
+  if (root.children.length > 0) {
+    //With the rating bumped up by one.
+    var subRoot = rootNode(root.name, root.data.rating + 1)
+    root.children.push(subRoot);
+    App.Tree.makeSubTree(subRoot, problems);
+  }
+  // go one level deeper into tree if problem children nonempty
+};
+
+// This might need changing:
+App.Tree.nid = 0;
+
+App.Tree.rootNode = function(name, rating) {
+  var node = {};
+  node['id'] = ++App.Tree.nid;
+  node['name'] = name;
+  node['data'] = { 'rating': (rating || 0) };
+  node['children']= [];
+  return node;
+};
+
+App.Tree.problemNode = function(problem) {
+  var node = {}
+  node['id'] = problem.id;
+  node['name'] = problem.title;
+  node['data'] = {'description': problem.description};
+  node['children'] = [];
+  return node;
+};
