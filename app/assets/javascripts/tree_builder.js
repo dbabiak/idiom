0e27fb3e499c928009fe2b47cf760bb6a95f30db
d-babiak <d.babiak@gmail.com>
2014-07-26 17:28:26 -0700

    .

--- app/assets/javascripts/tree_builder.js
+++ app/assets/javascripts/tree_builder.js
@@ -1,15 +1,12 @@
-//returns a nested json blob for parsing by the visualization library
 App.Tree = function(problems, options) {
-  var root = {
-    'id': '0',
-    'name': 'root',
-    'data': {},
-    'children': []
-  };
+  var root = App.Tree.rootNode()
 
-  var subRoots = ['recursion', 'fundamentals', 'algorithms', 'data-structures', 'logic'];
-  subRoots.forEach(function(name) {
-    var subRoot = App.Tree.rootNode(name, 0);
+  var categories = ['recursion', 'fundamentals', 'algorithms', 'data-structures', 'logic'];
+  categories.forEach(function(category) {
+    var subRoot = App.Tree.rootNode({
+      category: category,
+      rating: 0
+    });
     root.children.push(subRoot);
     App.Tree.makeSubTree(subRoot, problems);
   });
@@ -20,13 +17,17 @@ App.Tree = function(problems, options) {
 App.Tree.makeSubTree = function(root, problems) {
   // add all problem children
   problems.forEach(function(problem) {
-    if (problem.rating === root.data.rating && problem.category === root.name) {
+    if (problem.rating === root.rating && problem.category === root.category) {
       root.children.push(App.Tree.problemNode(problem));
     }
   });
+  // debugger;
   if (root.children.length > 0) {
     //With the rating bumped up by one.
-    var subRoot = App.Tree.rootNode(root.name, root.data.rating + 1)
+    var subRoot = App.Tree.rootNode({
+      category: root.category, 
+      rating: root.rating + 1
+    });
     root.children.push(subRoot);
     App.Tree.makeSubTree(subRoot, problems);
   }
@@ -36,50 +37,79 @@ App.Tree.makeSubTree = function(root, problems) {
 // This might need changing:
 App.Tree.nid = 0;
 
-App.Tree.rootNode = function(name, rating) {
+App.Tree.rootNode = function(options) {
+  options = options || {};
   var node = {};
-  node['id'] = "_" + (++App.Tree.nid);
-  node['name'] = name;
-  node['data'] = { 'rating': (rating || 0) };
-  node['children']= [];
+  node.id = "_" + (++App.Tree.nid);
+  node.rating = options.rating;
+  node.category = options.category;
+  node.children = [];
+  node.name = (node.category + ': ' + options.rating || 'big root');   
   return node;
 };
 
 App.Tree.problemNode = function(problem) {
   var node = {}
-  node['id'] = problem.id;
-  node['name'] = problem.title;
-  node['data'] = {'description': problem.description};
-  node['children'] = [];
+  node.id = problem.id;
+  node.name = problem.title;
+  node.description = problem; 
+  node.children = [];
   return node;
 };
 
+
 var RFT = App.RatingFirstTree = function(problems) {
-  var root = {
-    'id': '0',
-    'name': 'root',
-    'data': {},
-    'children': []
-  };
+  var root = rootNode();
 
   var ratings = [0, 1, 2, 3]
   var categories = ['recursion', 'fundamentals', 'algorithms', 'data-structures', 'logic'];
   ratings.forEach(function(rating) {
     var subRoot = App.RatingFirstTree.rootNode(rating);
     root.children.push(subRoot);
-    App.Tree.makeSubTree(subRoot, problems);
+    App.RatingFirstTree.makeSubTree(subRoot, problems, categories);
   });
-
   return root;
 }
 
+App.RatingFirstTree.nid = 0;
 //What do we get by having it be a constructor? 
 //this is simply a bundle of information
-App.RatingFirstTree.rootNode = function(rating) {
+App.RatingFirstTree.rootNode = function(options) {
+  options = options || {};
   var node = {};
-  node.id = "_" + (++App.Tree.nid);
-  node.name = name;
-  node.rating = (rating || 0)
+  node.id = "_" + (++App.RatingFirstTree.nid);
+  node.rating = options.rating;
+  node.category = options.category;
   node.children = [];
   return node;
 };
+
+App.RatingFirstTree.problemNode = function(problem) {
+  var node = {}
+  node.id = problem.id;
+  node.name = problem.title;
+  node.description = problem.description;
+  node.children = [];
+  return node;
+};
+
+App.RatingFirstTree.makeSubTree = function(root, problems, categories) {
+  // add all problem children
+  //We want to add all the category subtrees. 
+  categories.forEach(function(category) {
+    var categoryRoot = App.RatingFirstTree.rootNode({
+      category: category,
+      rating: root.rating
+    });
+    root.children.push(categoryRoot);
+    App.RatingFirstTree.makeCategorySubTree(categoryRoot, problems);
+  })
+};
+
+App.RatingFirstTree.makeCategorySubTree = function(categoryRoot, problems) {
+  problems.forEach(function(problem) {
+    if (problem.rating === categoryRoot.rating && problem.category === root.category) {
+      root.children.push(App.Tree.problemNode(problem));
+    }
+  });
+}
