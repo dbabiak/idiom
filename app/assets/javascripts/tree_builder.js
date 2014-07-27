8fb478356ab7f472da236e7325b79a357f104d84
Daniel Babiak <d.babiak@gmail.com>
2014-07-26 18:41:49 -0700

    more graph stuff

--- app/assets/javascripts/tree_builder.js
+++ app/assets/javascripts/tree_builder.js
@@ -1,54 +1,67 @@
-App.Tree = function(problems, options) {
-  var root = App.Tree.rootNode()
+var RootNode,
+    ProblemNode,
+    Tree;
+Tree = App.Tree = function(problems, options) {
+  var root = new RootNode({id: "root" + (Tree.nid++)})
+  root.name = 'root'
 
+  // Categories should be a class constant on problems.
   var categories = ['recursion', 'fundamentals', 'algorithms', 'data-structures', 'logic'];
   categories.forEach(function(category) {
-    var subRoot = App.Tree.rootNode({
+    var subRoot = new RootNode({
+      id: "root" + (Tree.nid++),
       category: category,
       rating: 0
     });
     root.children.push(subRoot);
-    App.Tree.makeSubTree(subRoot, problems);
+    Tree.makeSubTree(subRoot, problems);
   });
-
   return root;
 }
 
-App.Tree.makeSubTree = function(root, problems) {
+//Initialize id - this might need changing.
+Tree.nid = 0;
+
+Tree.makeSubTree = function(root, problems) {
   // add all problem children
   problems.forEach(function(problem) {
-    if (problem.rating === root.rating && problem.category === root.category) {
-      root.children.push(App.Tree.problemNode(problem));
+    if (problem.rating === root.data.rating &&
+        problem.category === root.data.category) {
+      root.children.push(new ProblemNode(problem));
     }
   });
   // debugger;
   if (root.children.length > 0) {
     //With the rating bumped up by one.
-    var subRoot = App.Tree.rootNode({
-      category: root.category,
-      rating: root.rating + 1
+    var subRoot = new RootNode({
+      id: "_" + (++Tree.nid),
+      category: root.data.category,
+      rating: root.data.rating + 1
     });
     root.children.push(subRoot);
-    App.Tree.makeSubTree(subRoot, problems);
+    Tree.makeSubTree(subRoot, problems);
   }
-  // go one level deeper into tree if problem children nonempty
 };
 
-// This might need changing:
-App.Tree.nid = 0;
-
-App.Tree.rootNode = function(options) {
-  options = options || {};
-  var node = {};
-  node.id = "_" + (++App.Tree.nid);
-  node.rating = options.rating;
-  node.category = options.category;
-  node.children = [];
-  node.name = (node.category + ': ' + options.rating || 'big root');
-  return node;
+var RootNode = function(options) {
+  this.id = options.id;
+  this.name = (options.category + ': ' + options.rating);
+  this.children = [];
+  this.data = {
+    isRoot: true,
+    category: options.category,
+    rating: options.rating
+  }
 };
 
-App.Tree.problemNode = function(problem) {
+var ProblemNode = function(problem) {
+  this.id = problem.id;
+  this.name = problem.title;
+  this.description = problem;
+  this.children = [];
+}
+
+Tree.problemNode = function(problem) {
   var node = {}
   node.id = problem.id;
   node.name = problem.title;
@@ -109,7 +122,7 @@ App.RatingFirstTree.makeSubTree = function(root, problems, categories) {
 App.RatingFirstTree.makeCategorySubTree = function(categoryRoot, problems) {
   problems.forEach(function(problem) {
     if (problem.rating === categoryRoot.rating && problem.category === root.category) {
-      root.children.push(App.Tree.problemNode(problem));
+      root.children.push(Tree.problemNode(problem));
     }
   });
 }
