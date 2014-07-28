5a94633487816ecdcb745a676d3b95c941e92b21
Daniel Babiak <d.babiak@gmail.com>
2014-07-28 10:32:01 -0700

    make sizing relative

--- app/assets/javascripts/views/solutions/show.js
+++ app/assets/javascripts/views/solutions/show.js
@@ -9,15 +9,31 @@ App.Views.SolutionView = Backbone.View.extend({
     className: 'solution-view',
 
     events: {
+      'click .upvote': 'upvote'
     },
 
   render: function() {
+    debugger;
     var content = this.template({
       solution: this.model,
       includeProblemLink: this.includeProblemLink
     });
     this.$el.html(content);
     return this;
+  },
+
+  upvote: function(event) {
+    // Make a custom ajax request.
+    event.preventDefault();
+    $.ajax({
+        url: "/api/solution_likes",
+        type: "POST",
+        data: { solution_like: {solution_id: this.model.id} },
+        success: function (response) {
+          //change color of 'like' button'
+          debugger;
+        }
+      });
   }
 
 });
--- app/controllers/api/solution_likes_controller.rb
+++ app/controllers/api/solution_likes_controller.rb
@@ -1,13 +1,14 @@
 class Api::SolutionLikesController < ApplicationController
   def create
     @solution_like = SolutionLike.new(solution_like_params)
+    @solution_like.user_id ||= current_user.id
     if SolutionLike.find_by(solution_like_params).nil? && @solution_like.save
       render json: @solution_like
     else
       render json: @solution_like.errors, status: :unprocessable_entity
     end
   end
-  
+
   def solution_like_params
     params.require(:solution_like).permit(:user_id, :solution_id)
   end
